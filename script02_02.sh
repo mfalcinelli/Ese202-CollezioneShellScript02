cfba1c0b977f77adb57faeabb012d3ed67fb26a1
mfalcinelli <falcio97@gmail.com>
2015-03-09 19:16:45 +0100

    Update script02_02.sh

--- script02_02.sh
+++ script02_02.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+if [ $# -gt 1 -o $# -eq 0 ]
+then
+	echo
+	echo "Errore! Inserire uno e un solo argomento!"
+	exit 1
+fi
+for i in $(seq 10)
+do
+	mkdir $@$i
+done
+
+exit 0
 
 #
 # Questo script riceve un argomento. Esso contiene un _prefisso_ per il nome di una directory.
